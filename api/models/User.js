444ebb9d00139fad2ddfd9e95bdc12b2147a4967
Arman Sani <armanisani@gmail.com>
2016-04-04 11:46:12 -0700

    server.js file added routes and user model

--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,17 @@
 var
   mongoose = require('mongoose'),
-  schema = mongose.Schema,
-  ObjectId = Schema.Types.ObjectId
+  Schema = mongose.Schema,
+  ObjectId = Schema.Types.ObjectId,
+  // bcrypt = require('bcrypt-nodejs')
 
 var user_schema = Schema({
   name: string,
-  username: {type: String, unique: true, }
+  username: {type: String, unique: true, required: true },
+  password: {type: String, required: true},
 })
+
+
+var User = mongoose.model('User', userSchema)
+
+
+module.exports = User
--- api/server.js
+++ api/server.js
@@ -5,7 +5,14 @@ var
   body_parser = require('body-parser'),
   morgan = require('morgan'),
   config = require('./config.js'),
-  port = process.env.PORT || 8100
+  port = process.env.PORT || 8100,
+  // bcrypt = require('bcrypt-nodejs'),
+  userRoutes = require('./routes/users.js'),
+  likesRoutes = require('./routes/likes.js'),
+  productsRoutes = require('./routes/products.js'),
+  categoryRoutes = require('./routes/categories.js')
+
+
 
 mongoose.connect(config.databaseUrl, function(err){
   if(err) throw err
@@ -16,6 +23,11 @@ app.use(body_parser.urlencoded({extended: false}))
 app.use(body_parser.json())
 
 app.use(morgan("dev"))
+
+app.use('/users', userRoutes)
+app.use('/likes', likesRoutes)
+app.use('/products', productsRoutes)
+app.use('/category', categoryRoutes)
 
 app.listen(port, function(err){
   if(err) throw err
