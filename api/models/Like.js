63a61d0cb5c77a259b4f12148fcfcab1979b8ddf
Jennifer W <w.jennifer123@gmail.com>
2016-04-04 11:53:35 -0700

    made like model

---
+++ api/models/Like.js
@@ -0,0 +1,13 @@
+var
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId
+
+var like_schema = Schema({
+        user: {type: ObjectId, ref: "User"},
+        product: {type: ObjectId, ref: "Product"}
+    })
+
+var Like = mongoose.model('Like', like_schema)
+
+module.exports = Like
