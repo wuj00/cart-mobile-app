e9c84f6d23c79c7c3d5b4d3fb222f56c9ca64e75
lewhub <bracey.lewis@gmail.com>
2016-04-04 19:22:51 -0700

    an done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,11 @@
 angular.module('starter.controllers', [])
+  .controller('HomeCtrl', HomeCtrl)
+  .controller('SearchCtrl', SearchCtrl)
+  .controller('PostCtrl', PostCtrl)
+  .controller('NotificationsCtrl', NotificationsCtrl)
+  .controller('ProfileCtrl', ProfileCtrl)
 
-.controller('HomeCtrl', function($scope) {
-  
-})
-
-.controller('SearchCtrl', function($scope) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  //
-  // $scope.chats = Chats.all();
-  // $scope.remove = function(chat) {
-  //   Chats.remove(chat);
-  // };
-})
-
-.controller('PostCtrl', function($scope) {
-  // $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('NotificationsCtrl', function($scope) {
-  // $scope.settings = {
-  //   enableFriends: true
-  // }
-})
-
-.controller('ProfileCtrl', function($scope){
-
-})
+function HomeCtrl(){
+  var self = this
+  self.title = "This is the home ctrl title"
+}
