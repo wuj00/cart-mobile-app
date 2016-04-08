f4d91db0ccbdd8020cbe78de39edeb45e8c087f9
Jennifer W <w.jennifer123@gmail.com>
2016-04-08 04:30:55 -0700

    fixed styling for search page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -154,6 +154,7 @@ function HomeCtrl($stateParams, userService, productService, likeService, $windo
   // Setup the loader
   $ionicLoading.show({
     animation: 'fade-in',
+    duration: 10000,
     showBackdrop: true,
     maxWidth: 200,
     showDelay: 0,
